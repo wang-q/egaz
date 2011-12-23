e64083dce6ced57912279145c487ded4ab68b418
wangq <wangqiang1997@gmail.com>
2011-12-23 21:56:54 +0800

    Redesign json outputs

--- lav2json.pl
+++ lav2json.pl
@@ -14,6 +14,8 @@ use File::Slurp;
 my $lavfile;
 my $output;
 
+my $pretty;
+
 my $man  = 0;
 my $help = 0;
 
@@ -22,6 +24,7 @@ GetOptions(
     'man'         => \$man,
     'l|lavfile=s' => \$lavfile,
     'o|output=s'  => \$output,
+    'p|pretty'    => \$pretty,
 ) or pod2usage(2);
 
 pod2usage(1) if $help;
@@ -36,7 +39,8 @@ unless ($output) {
 #----------------------------------------------------------#
 # now run!
 #----------------------------------------------------------#
-my $coder     = JSON::XS->new->ascii->pretty->allow_nonref;
+my $coder = JSON::XS->new->ascii->allow_nonref;
+$coder->pretty if $pretty;
 my $full_json = {};
 
 my $lav_content = read_file($lavfile);
@@ -47,6 +51,8 @@ shift @lavs;    # .lav file start with #:lav
 # d-stanza
 #----------------------------#
 {
+    $full_json->{d} = {};
+
     my $d_stanza = shift @lavs;
     $d_stanza =~ /d {\s+(.+?)\s+}/s;
     $d_stanza = $1;
@@ -56,19 +62,22 @@ shift @lavs;    # .lav file start with #:lav
 
     my %opt_of;
     my $opt_lines = $line_first . $line_last;
+    if ( $opt_lines =~ /^"([a-zA-z0-9\.\-]+)/ ) {
+        $opt_of{exe} = $1;
+    }
     while ( $opt_lines =~ /\b(\w)\s?=\s?(\d+)\b/g ) {
         $opt_of{$1} = $2;
     }
-    $full_json->{header}{opt} = \%opt_of;
+    $full_json->{d}{opt} = \%opt_of;
 
     my $score = join "\n", @lines;
-    $full_json->{header}{score} = $score;
+    $full_json->{d}{score} = $score;
 }
 
 $full_json->{runs} = [];
 foreach my $lav (@lavs) {
-    my $run_json   = {};
-    my $run_header = {};
+    my $run_json = {};
+    my $run_header = { t => {}, q => {} };
 
     #----------------------------#
     # s-stanza
@@ -80,19 +89,17 @@ foreach my $lav (@lavs) {
         unless ( ( scalar @s_lines ) == 2 ) { die "s-stanza error.\n"; }
 
         $s_lines[0] =~ /\s*\"?(.+?)\-?\"? (\d+) (\d+) (\d+) (\d+)/;
-        $run_header->{t_file}   = $1;
-        $run_header->{t_from}   = $2;
-        $run_header->{t_to}     = $3;
-        $run_header->{t_strand} = $4 ? '-' : '+';
-        $run_header->{t_number} = $5;
+        $run_header->{t}{file}   = $1;
+        $run_header->{t}{range}  = "$2-$3";
+        $run_header->{t}{strand} = $4 ? '-' : '+';
+        $run_header->{t}{serial} = $5;
 
         $s_lines[1] =~ /\s*\"?(.+?)\-?\"? (\d+) (\d+) (\d+) (\d+)/;
         my ( $q_file, $q_strand, $q_contig ) = ( $1, $2, $3 );
-        $run_header->{q_file}   = $1;
-        $run_header->{q_from}   = $2;
-        $run_header->{q_to}     = $3;
-        $run_header->{q_strand} = $4 ? '-' : '+';
-        $run_header->{q_number} = $5;
+        $run_header->{q}{file}   = $1;
+        $run_header->{q}{range}  = "$2-$3";
+        $run_header->{q}{strand} = $4 ? '-' : '+';
+        $run_header->{q}{serial} = $5;
     }
 
     #----------------------------#
@@ -105,12 +112,12 @@ foreach my $lav (@lavs) {
         unless ( ( scalar @h_lines ) == 2 ) { die "h-stanza error.\n"; }
 
         $h_lines[0] =~ /\s*\"\>(.+)\"/;
-        $run_header->{t_name} = $1;
-        $run_header->{t_name} =~ s/ \(reverse complement\)//;
+        $run_header->{t}{name} = $1;
+        $run_header->{t}{name} =~ s/ \(reverse complement\)//;
 
         $h_lines[1] =~ /\s*\"\>(.+)\"/;
-        $run_header->{q_name} = $1;
-        $run_header->{q_name} =~ s/ \(reverse complement\)//;
+        $run_header->{q}{name} = $1;
+        $run_header->{q}{name} =~ s/ \(reverse complement\)//;
     }
 
     $run_json->{header} = $run_header;
@@ -134,30 +141,28 @@ foreach my $lav (@lavs) {
         unless ( $a_stanza =~ /\s*b (\d+) (\d+)/ ) {
             die "There isn't a s-line.\n";
         }
-        $align_json->{t_from} = $1;
-        $align_json->{q_from} = $2;
+        my $t_from = $1;
+        my $q_from = $2;
 
         # e-line, ends
         unless ( $a_stanza =~ /\s*e (\d+) (\d+)/ ) {
             die "There isn't a e-line.\n";
         }
-        $align_json->{t_to} = $1;
-        $align_json->{q_to} = $2;
+        my $t_to = $1;
+        my $q_to = $2;
 
-        $align_json->{blocks} = [];
+        $align_json->{t} = "$t_from-$t_to";
+        $align_json->{q} = "$q_from-$q_to";
 
-        my @align_pieces = $a_stanza =~ /\s*l (\d+ \d+ \d+ \d+) \d+/g;
+        $align_json->{blocks} = { t => [], q => [], i => [], };
+        my @align_pieces = $a_stanza =~ /\s*l (\d+ \d+ \d+ \d+ \d+)/g;
         foreach my $align_piece (@align_pieces) {
-            unless ( $align_piece =~ /(\d+) (\d+) (\d+) (\d+)/ ) {
+            unless ( $align_piece =~ /(\d+) (\d+) (\d+) (\d+) (\d+)/ ) {
                 die "l-line error\n";
             }
-            my $block_json = {};
-            $block_json->{t_begin} = $1;
-            $block_json->{q_begin} = $2;
-            $block_json->{t_end}   = $3;
-            $block_json->{q_end}   = $4;
-
-            push @{ $align_json->{blocks} }, $block_json;
+            push @{ $align_json->{blocks}{t} }, "$1-$3";
+            push @{ $align_json->{blocks}{q} }, "$2-$4";
+            push @{ $align_json->{blocks}{i} }, "$5";
         }
 
         push @{ $run_json->{aligns} }, $align_json;
@@ -167,7 +172,6 @@ foreach my $lav (@lavs) {
 }
 
 my $text = $coder->encode($full_json);
-
 open my $fh, '>', $output;
 print {$fh} $text;
 close $fh;
@@ -182,14 +186,13 @@ __END__
 
 =head1 SYNOPSIS
 
-    lav2json.pl -l <lavfile> -o <output>
-
-    lav2axt.pl [options]
-     Options:
-       -h, --help               brief help message
-       -m, --man                full documentation
-       -l, --lavfile
-       -o, --output
+    lav2json.pl -l <lavfile> -o <output> [options]
+      Options:
+        -h, --help              brief help message
+        -m, --man               full documentation
+        -l, --lavfile
+        -o, --output
+        -p, --prety
 
 =head1 OPTIONS
 

43802a6d6dfb0257a6d28b55d63e792b41b7645b
wangq <wangqiang1997@gmail.com>
2012-02-04 05:17:58 +0800

    Run multiz sequencially

---
+++ mz.pl
@@ -0,0 +1,400 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use Getopt::Long;
+use Pod::Usage;
+use YAML qw(Dump Load DumpFile LoadFile);
+
+use AlignDB::Run;
+use AlignDB::Util qw(:all);
+
+use Bio::Phylo::IO qw(parse);
+use Set::Scalar;
+use List::Flatten;
+use List::MoreUtils qw(uniq zip);
+use Time::Duration;
+use File::Find::Rule;
+use File::Spec;
+use File::Basename;
+use File::Remove qw(remove);
+use Path::Class;
+use String::Compare;
+
+use FindBin;
+
+#----------------------------------------------------------#
+# GetOpt section
+#----------------------------------------------------------#
+# executable file location
+my $kent_bin   = "~/bin/x86_64";
+my $multiz_bin = "~/bin";
+
+# dirs of *pairwise* maf
+my @dirs;
+
+# newick tree
+# Must be a rooted tree
+my $tree_file;
+
+#  names
+my $target_name;
+
+# .synNet.maf or .net.maf
+my $syn;
+
+# run in parallel mode
+my $parallel = 1;
+
+my $man  = 0;
+my $help = 0;
+
+GetOptions(
+    'help|?'         => \$help,
+    'man'            => \$man,
+    'bin|kent_bin=s' => \$kent_bin,
+    'multiz_bin=s'   => \$multiz_bin,
+    'd|dir=s'        => \@dirs,
+    'tree=s'         => \$tree_file,
+    'target=s'       => \$target_name,
+    'syn'            => \$syn,
+    'p|parallel=i'   => \$parallel,
+) or pod2usage(2);
+
+pod2usage(1) if $help;
+pod2usage( -exitstatus => 0, -verbose => 2 ) if $man;
+
+#----------------------------------------------------------#
+# Init
+#----------------------------------------------------------#
+my $start_time = time;
+print "\n", "=" x 30, "\n";
+print "Processing...\n";
+
+my $suffix = $syn ? '.synNet.maf' : '.net.maf';
+my @files = File::Find::Rule->file->name("*$suffix")->in(@dirs);
+printf "\n----%4s $suffix files ----\n", scalar @files;
+
+#----------------------------#
+# Gather species list from maf and tree
+#----------------------------#
+my @species;
+my %species_of;
+my $number_of_chr;
+{
+    print "Get species list\n";
+    for (@files) {
+        my $cmd = "$kent_bin/mafSpeciesList $_ stdout";
+        $species_of{$_} = [ grep {$_} split /\n/, `$cmd` ];
+    }
+
+    # count
+    my %seen;
+    for my $key ( keys %species_of ) {
+        for my $elem ( @{ $species_of{$key} } ) {
+            $seen{$elem}++;
+        }
+    }
+    (@species) = map { $_->[0] }
+        sort { $b->[1] <=> $a->[1] }
+        map { [ $_, $seen{$_} ] }
+        keys %seen;
+
+    # check species number
+    my $number = scalar @species;
+    die "There're only $number species, [@species].\nCan't run multiz.\n"
+        if $number < 3;
+
+    if ( !$target_name ) {
+        printf "%s appears %d times, use it as target.\n", $species[0],
+            $seen{ $species[0] };
+        $target_name = $species[0];
+        shift @species;
+    }
+    else {
+        my ($dummy) = grep { $_ eq $target_name } @species;
+        die "Can't find target_name [$target_name] in .maf files.\n"
+            unless $dummy;
+        @species = grep { $_ ne $target_name } @species;
+    }
+
+    # check other species occurrence number
+    my @occurrence = uniq( @seen{@species} );
+    die "Species occurrence number inconsistency [@occurrence]\n"
+        if @occurrence > 1;
+    $number_of_chr = $occurrence[0];
+    print "I guess there are $number_of_chr chromosomes.\n";
+
+    # sort @species by distances in tree
+    my $ladder     = ladder( $tree_file, $target_name );
+    my @order      = flat( @{$ladder} );
+    my %item_order = map { $order[$_] => $_ } 0 .. $#order;
+
+    @species = map { $_->[0] }
+        sort { $a->[1] <=> $b->[1] }
+        map { [ $_, $item_order{$_} ] } @species;
+
+    print "Order of stitch [@species]\n";
+
+    my $cross = @species * $number_of_chr;
+    if ( $cross == @files ) {
+        print "Perfect! All maf files ara pairwised.\n";
+    }
+    elsif ( $cross > @files ) {
+        die "There are multiple species maf files. Not sure it works\n";
+    }
+    else {
+        die "Please check maf files. It seemed there are redundances.\n";
+    }
+}
+
+#----------------------------#
+# matching mafs
+#----------------------------#
+#---
+#Bur_0:
+#  1: ~/data/alignment/arabidopsis19/AthvsBur_0/mafNet/chr1.net.maf
+#  2: ...
+#lyrata_65:
+#  1: ~/data/alignment/arabidopsis19/AthvsLyrata_set01_4/mafNet/chr1.net.maf
+#  2: ...
+my $file_of = {};
+my @chr_names;
+{
+    my @chr_string;
+    for my $name (@species) {
+        my @species_files;
+        for my $key (%species_of) {
+            my ($dummy) = grep { $_ ne $target_name } @{ $species_of{$key} };
+            if ( $dummy and $name eq $dummy ) {
+                push @species_files, $key;
+            }
+        }
+
+        my @chrs = map { basename $_ , $suffix }
+            @species_files;    # strip dir and suffix
+        while (1) {
+            my $lcss = lcss(@chrs);
+            last unless $lcss;
+            print "LCSS [$lcss]\n";
+            my $rx = quotemeta $lcss;
+            $chrs[$_] =~ s/$rx// for 0 .. $#chrs;
+        }
+        $file_of->{$name} = { zip( @chrs, @species_files ) };
+        push @chr_string, join " ", sort @chrs;
+    }
+
+    if ( scalar uniq(@chr_string) == 1 ) {
+        @chr_names = split / /, $chr_string[0];
+        if ( @chr_names == $number_of_chr ) {
+            print "Check maf filenames OK\n";
+            print "@chr_names\n";
+        }
+        else {
+            print "chr_string\t@chr_string\n";
+            print "number_of_chr\t$number_of_chr\n";
+            die "Check maf filenames FAILED\n";
+        }
+    }
+    else {
+        print "@chr_string\n";
+        die "Check maf filenames FAILED\n";
+    }
+}
+
+#----------------------------#
+# Finally, multiz comes
+#----------------------------#
+
+my $worker = sub {
+    my $job = shift;
+    my $opt = shift;
+
+    my $chr_name = $job;
+
+# multiz.v11.2: -- aligning two files of alignment blocks where top rows are
+# always the reference, reference in both files cannot have duplicats
+# args: [R=?] [M=?] file1 file2 v? [out1 out2] [nohead] [all]
+#         R(30) radius in dynamic programming.
+#         M(1) minimum output width.
+#         out1 out2(null) null: stdout; out1 out2: file names for collecting unused input.
+#         nohead(null) null: output maf header; nohead: not to output maf header.
+#         all(null) null: not to output single-row blocks; all: output all blocks.
+#
+# multiz ~/data/alignment/arabidopsis19/AthvsZu_0/mafSynNet/chr1.synNet.maf \
+#       ~/data/alignment/arabidopsis19/AthvsNo_0/mafSynNet/chr1.synNet.maf \
+#       1 out1 out2 > step1.chr1.maf
+# multiz step1.chr1.maf ~/data/alignment/arabidopsis19/AthvsBur_0/mafSynNet/chr1.synNet.maf \
+#       1 out1 out2 > step2.chr1.maf
+# multiz step2.chr1.maf ~/data/alignment/arabidopsis19/AthvsLyrata_set01_4/mafSynNet/chr1.synNet.maf
+#       1 out1 out2 > step3.chr1.maf
+
+    my @species_copy = @species;
+    my ( $species1, $species2 );
+    my $maf_step;
+    my $step = 1;
+    while (@species_copy) {
+        my ( $maf1, $maf2 );
+
+        if ( !defined $species1 ) {
+            $species1 = shift @species_copy;
+            $maf1     = $file_of->{$species1}{$chr_name};
+        }
+        else {
+            $maf1 = $maf_step;
+        }
+        if ( !defined $species2 ) {
+            $species2 = shift @species_copy;
+            $maf2     = $file_of->{$species2}{$chr_name};
+        }
+
+        $maf_step
+            = @species_copy
+            ? "chr$chr_name.step$step$suffix"
+            : "chr$chr_name$suffix";
+
+        print "Run multiz...\n";
+        my $cmd
+            = "$multiz_bin/multiz" 
+            . " $maf1" 
+            . " $maf2"
+            . " 1 out1 out2"
+            . " > $maf_step";
+        exec_cmd($cmd);
+        print ".maf file generated.\n\n";
+
+        $species1 = "Occupied";
+        $species2 = undef;
+        $step++;
+    }
+
+    return;
+};
+
+my $run = AlignDB::Run->new(
+    parallel => $parallel,
+    jobs     => \@chr_names,
+    code     => $worker,
+);
+$run->run;
+
+print "\n";
+print "Runtime ", duration( time - $start_time ), ".\n";
+print "=" x 30, "\n";
+
+exit;
+
+#----------------------------------------------------------#
+# Subroutines
+#----------------------------------------------------------#
+sub exec_cmd {
+    my $cmd = shift;
+
+    print "\n", "-" x 12, "CMD", "-" x 15, "\n";
+    print $cmd , "\n";
+    print "-" x 30, "\n";
+
+    #system $cmd;
+}
+
+#----------------------------#
+# parsing tree
+#----------------------------#
+sub ladder {
+    my $file        = shift;
+    my $target_name = shift;
+
+    my $tree = parse( -format => 'newick', -file => $file )->first;
+
+    my $target = $tree->get_by_name($target_name);
+
+    my @ladder = ( [$target_name], );
+    my $set = Set::Scalar->new;
+    $set->insert($target_name);
+
+    my $start = $target;
+    while (1) {
+        my $parent = $start->get_parent;
+        last unless $parent;
+        my @nodes
+            = grep { !$set->has( $_->get_name ) } @{ $parent->get_terminals };
+        $set->insert( $_->get_name ) for @nodes;
+        my $distance_of = {};
+        for my $node (@nodes) {
+            $distance_of->{ $node->get_name }
+                = $node->calc_patristic_distance($target);
+        }
+        my @sorted = map { $_->[0] }
+            sort { $a->[1] <=> $b->[1] }
+            map { [ $_, $distance_of->{$_} ] }
+            keys %{$distance_of};
+        push @ladder, [@sorted];
+
+        $start = $parent;
+    }
+
+    return \@ladder;
+}
+
+# comes from
+# http://stackoverflow.com/questions/499967/how-do-i-determine-the-longest-similar-portion-of-several-strings
+sub lcss {
+    return '' unless @_;
+    return $_[0] if @_ == 1;
+    my $i          = 0;
+    my $first      = shift;
+    my $min_length = length($first);
+    for (@_) {
+        $min_length = length($_) if length($_) < $min_length;
+    }
+INDEX: for my $ch ( split //, $first ) {
+        last INDEX unless $i < $min_length;
+        for my $string (@_) {
+            last INDEX if substr( $string, $i, 1 ) ne $ch;
+        }
+    }
+    continue { $i++ }
+    return substr $first, 0, $i;
+}
+
+__END__
+
+=head1 NAME
+
+    amp.pl - axt-maf-phast pipeline
+
+=head1 SYNOPSIS
+
+    amp.pl -dt <one target dir or file> -dq <one query dir or file> [options]
+      Options:
+        -?, --help              brief help message
+        --man                   full documentation
+
+      Run in parallel mode
+        -p, --paralle           number of child processes
+
+      Fasta dirs  
+        -dt, --dir_target       dir of target fasta files
+        -dq, --dir_query        dir of query fasta files
+
+      Output .lav and .axt
+        -dl, --dir_lav          where .lav and .axt files storess
+
+=head1 OPTIONS
+
+=over 4
+
+=item B<-help>
+
+Print a brief help message and exits.
+
+=item B<-man>
+
+Prints the manual page and exits.
+
+=back
+
+=head1 DESCRIPTION
+
+
+=cut

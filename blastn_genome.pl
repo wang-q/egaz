0b06eea210f8b12b8e8225b902c5fd0dea1ff184
wang-q <wang-q@outlook.com>
2015-12-29 08:03:39 +0800

    Parallelised by MCE::Flow

--- blastn_genome.pl
+++ blastn_genome.pl
@@ -8,8 +8,10 @@ use FindBin;
 use YAML qw(Dump Load DumpFile LoadFile);
 
 use Path::Tiny;
-use Bio::SearchIO;
 use Set::Scalar;
+
+use MCE;
+use MCE::Flow Sereal => 1;
 
 use AlignDB::IntSpan;
 use AlignDB::Stopwatch;
@@ -45,17 +47,21 @@ blastn_genome.pl - Get more paralog pieces by genomic blasting
         --coverage      -c  FLOAT   default is [0.95]       
         --genome        -g  STR     reference genome file
         --output        -o  STR     output
+        --parallel      -p  INT     default is [8]
+        --chunk_size        INT     default is [10000]
 
 =cut
 
 GetOptions(
     'help|?'   => sub { HelpMessage(0) },
-    'file|f=s' => \my $file,
+    'file|f=s' => \( my $file ),
     'view|m=s'     => \( my $alignment_view = 0 ),
     'identity|i=i' => \( my $identity       = 90 ),
     'coverage|c=f' => \( my $coverage       = 0.95 ),
-    'genome|g=s'   => \my $genome,
-    'output|o=s'   => \my $output,
+    'genome|g=s'   => \( my $genome ),
+    'output|o=s'   => \( my $output ),
+    'parallel|p=i' => \( my $parallel       = 8 ),
+    'chunk_size=i' => \( my $chunk_size     = 10000 ),
 ) or HelpMessage(1);
 
 if ( !defined $file ) {
@@ -96,83 +102,89 @@ $stopwatch->start_message("Find paralogs...");
 #----------------------------------------------------------#
 $stopwatch->block_message("load blast reports");
 
-my %locations;    # genome locations
-open my $blast_fh, '<', $file;
-my $searchio = Bio::SearchIO->new(
-    -format => $result_format,
-    -fh     => $blast_fh,
-);
+my $worker = sub {
+    my ( $self, $chunk_ref, $chunk_id ) = @_;
 
-QUERY: while ( my $result = $searchio->next_result ) {
-    my $query_name = $result->query_name;
+    my $wid = MCE->wid;
+    print "Process task [$chunk_id] by worker #$wid\n";
 
-    # blasttable don't have $result->query_length
-    my $query_info   = decode_header($query_name);
-    my $query_length = $query_info->{chr_end} - $query_info->{chr_start} + 1;
-    print "Name $query_name\tLength $query_length\n";
-    while ( my $hit = $result->next_hit ) {
-        my $hit_name = $hit->name;
+    my @lines = @{$chunk_ref};
+    printf "%s\n", scalar @lines;
+    my %heads;
+    for my $line (@lines) {
+        next if $line =~ /^#/;
+        chomp $line;
 
-        while ( my $hsp = $hit->next_hsp ) {
-            my $query_set = AlignDB::IntSpan->new;
+        # Query id, Subject id, %identity, alignment length, mismatches, gap openings,
+        # q.start, q.end, s. start, s. end, e-value, bit score
+        my @fields = grep {defined} split /\s+/, $line;
+        if ( @fields != 12 ) {
+            print "Fields error: $line\n";
+            next;
+        }
 
-            # process the Bio::Search::HSP::HSPI object
-            my $hsp_length = $hsp->length('query');
+        my $query_name = $fields[0];
+        my $hit_name   = $fields[1];
 
-            # use "+" for default strand
-            # -1 = Minus strand, +1 = Plus strand
-            my ( $query_strand, $hit_strand ) = $hsp->strand("list");
-            my $hsp_strand = "+";
-            if ( $query_strand + $hit_strand == 0 and $query_strand != 0 ) {
-                $hsp_strand = "-";
+        my $query_info   = decode_header($query_name);
+        my $query_length = $query_info->{chr_end} - $query_info->{chr_start} + 1;
+
+        print " " x 4 . "Name $query_name\tLength $query_length\n";
+
+        my ( $q_start, $q_end ) = ( $fields[6], $fields[7] );
+        if ( $q_start > $q_end ) {
+            ( $q_start, $q_end ) = ( $q_end, $q_start );
+        }
+        my $query_set = AlignDB::IntSpan->new;
+        $query_set->add_pair( $q_start, $q_end );
+
+        my ( $h_start, $h_end ) = ( $fields[8], $fields[9] );
+        if ( $h_start > $h_end ) {
+            ( $h_start, $h_end ) = ( $h_end, $h_start );
+        }
+
+        my $query_coverage = $query_set->size / $query_length;
+        my $hsp_identity   = $fields[2];
+
+        #print Dump {
+        #    hit_name       => $hit_name,
+        #    hsp_identity   => $hsp_identity,
+        #    q_start        => $q_start,
+        #    q_end          => $q_end,
+        #    h_start        => $h_start,
+        #    h_end          => $h_end,
+        #    query_coverage => $query_coverage,
+        #    hsp_identity   => $hsp_identity,
+        #};
+
+        if ( $query_coverage >= $coverage and $hsp_identity >= $identity ) {
+            my $head = "$hit_name(+):$h_start-$h_end";
+            $heads{$head}++;
+
+            if (    $hit_name eq $query_info->{chr_name}
+                and $h_start eq $query_info->{chr_start}
+                and $h_end eq $query_info->{chr_end} )
+            {
+                print " " x 8 . "Find itself\n";
             }
-
-            my ( $q_start, $q_end ) = $hsp->range('query');
-            if ( $q_start > $q_end ) {
-                ( $q_start, $q_end ) = ( $q_end, $q_start );
-            }
-            $query_set->add_range( $q_start, $q_end );
-
-            my ( $h_start, $h_end ) = $hsp->range('hit');
-            if ( $h_start > $h_end ) {
-                ( $h_start, $h_end ) = ( $h_end, $h_start );
-            }
-
-            my $query_coverage = $query_set->size / $query_length;
-            my $hsp_identity   = $hsp->percent_identity;
-
-            #print Dump {
-            #    hit_name     => $hit_name,
-            #    hsp_identity => $hsp_identity,
-            #    q_start      => $q_start,
-            #    q_end        => $q_end,
-            #    h_start      => $h_start,
-            #    h_end        => $h_end,
-            #    query_strand => $query_strand,
-            #    hit_strand   => $hit_strand,
-            #    hsp_strand   => $hsp_strand,
-            #};
-
-            if ( $query_coverage >= $coverage and $hsp_identity >= $identity ) {
-                my $head = "$hit_name(+):$h_start-$h_end";
-
-                if (    $hit_name eq $query_info->{chr_name}
-                    and $h_start eq $query_info->{chr_start}
-                    and $h_end eq $query_info->{chr_end} )
-                {
-                    print " " x 4 . "Find itself\n";
-                }
-                else {
-
-                    print " " x 4 . "Find new [$head]\n";
-                    $locations{$head}++;
-                }
+            else {
+                print " " x 8 . "Find [$head]\n";
             }
         }
     }
-}
 
-close $blast_fh;
+    printf "Gather %d pieces\n", scalar keys %heads;
+    MCE->gather(%heads);
+};
+
+MCE::Flow::init {
+    chunk_size  => $chunk_size,
+    max_workers => $parallel,
+};
+my %locations = mce_flow_f $worker, $file;
+MCE::Flow::finish;
+
+$stopwatch->block_message( "Finish parse blast results", 1 );
 
 {
     #----------------------------#

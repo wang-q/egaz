914082054a5f4dc4fd575494ca3543727dfd5333
wang-q <wang-q@outlook.com>
2016-01-05 08:35:08 +0800

    Parallelise with MCE::Flow

--- proc_cc_chop.pl
+++ proc_cc_chop.pl
@@ -12,12 +12,16 @@ use Set::Scalar;
 use List::Util qw(max);
 use List::MoreUtils qw(minmax firstidx);
 
+use MCE;
+use MCE::Flow Sereal => 1;
+
 use AlignDB::IntSpan;
 use AlignDB::Stopwatch;
 use AlignDB::Util qw(multi_align multi_align_matrix trim_head_tail);
 
 use lib "$FindBin::RealBin/lib";
-use MyUtil qw(string_to_set revcom get_seq_faidx read_sizes decode_header change_name_chopped);
+use MyUtil
+    qw(string_to_set revcom get_seq_faidx read_sizes decode_header change_name_chopped sort_cc);
 
 #----------------------------------------------------------#
 # GetOpt section
@@ -25,28 +29,33 @@ use MyUtil qw(string_to_set revcom get_seq_faidx read_sizes decode_header change
 
 =head1 NAME
 
-merge_node.pl - merge overlapped nodes of paralog graph
+proc_cc_chop.pl - chopping connected paralog sequences
     
 =head1 SYNOPSIS
 
-    perl merge_node.pl -f <file> [options]
+    perl proc_cc_chop.pl -f <cc file> [options]
       Options:
         --help          -?          brief help message
         --file          -f  STR     file
         --size          -s  STR     chr.sizes
         --genome        -g  STR     reference genome file
-        --output        -o  STR     output   
+        --ratio         -r  FLOAT   report bad cc if lengths differences
+                                    large than this ratio, default is [0.8]
+        --output        -o  STR     output
         --msa               STR     Aligning program, default is [mafft]
+        --parallel      -p  INT     default is [8]
 
 =cut
 
 GetOptions(
     'help|?'     => sub { HelpMessage(0) },
-    'file|f=s'   => \my $cc_file,
-    'size|s=s'   => \my $size_file,
-    'genome|g=s' => \my $genome,
-    'output|o=s' => \my $output,
-    'msa=s' => \( my $msa = 'mafft' ),
+    'file|f=s'   => \( my $cc_file ),
+    'size|s=s'   => \( my $size_file ),
+    'genome|g=s' => \( my $genome ),
+    'ratio|r=f' => \( my $ratio = 0.8 ),
+    'output|o=s'   => \( my $output ),
+    'msa=s'        => \( my $msa = 'mafft' ),
+    'parallel|p=i' => \( my $parallel = 8 ),
 ) or HelpMessage(1);
 
 if ( !defined $size_file ) {
@@ -66,7 +75,6 @@ elsif ( !path($genome)->is_file ) {
 if ( !$output ) {
     $output = path($cc_file)->basename;
     ($output) = grep {defined} split /\./, $output;
-    $output = "$output.cc";
 }
 
 #----------------------------------------------------------#
@@ -82,166 +90,125 @@ $stopwatch->start_message("Analysis [$cc_file]");
 #----------------------------#
 # load cc
 #----------------------------#
-my $yml = LoadFile($cc_file);
+$stopwatch->block_message("Load cc [$cc_file]");
+my @cc           = @{ LoadFile($cc_file) };
+my $total_number = scalar @cc;
 
-my @cc       = @{ $yml->{cc} };
-my $count_of = $yml->{count};
-my @copies   = grep { $_ >= 2 } sort { $a <=> $b } keys %{$count_of};
-my @chrs     = keys %{ read_sizes($size_file) };
+{    # Remove alignment files if existing
+    my %seen;
+    for my $c (@cc) {
+        my $copy = scalar @{$c};
+        $seen{$copy}++;
+    }
+
+    for ( keys %seen ) {
+        path("$output.copy$_.fas")->remove;
+    }
+    path("$output.pairwise.fas")->remove;
+}
 
 #----------------------------#
 # write piece sequences
 #----------------------------#
-{
-    print "Write aligned cc sequences\n";
+$stopwatch->block_message("Write aligned cc sequences");
 
-    for (@copies) {
-        path("$output.copy$_.fas")->remove;
-    }
-    path("$output.pairwise.fas")->remove;
+my $file_bad_cc = "bad_cc.txt";
+path($file_bad_cc)->remove;
 
-    my @new_cc;
-    
-    my @bad_cc;
+my $worker = sub {
+    my ( $self, $chunk_ref, $chunk_id ) = @_;
 
-    print " " x 4 . "Get cc sequences\n";
-    for my $c (@cc) {
-        my $copy = scalar @{$c};
-        if ( $copy < 2 ) {
-            printf " " x 8 . "Single cc [%s]\n", $c->[0];
-            push @bad_cc, $c;
-            next;
-        }
-        print " " x 8 . "This cc has $copy copies\n";
-
-        # Get subseq from genomic files
-        print " " x 12 . "Get sequences from genomic files\n";
-        my @heads = @{$c};
-        my @seqs;
-        for my $head ( @{$c} ) {
-            my $info = decode_header($head);
-            my $location = sprintf "%s:%d-%d", $info->{chr_name}, $info->{chr_start},
-                $info->{chr_end};
-            my $seq = get_seq_faidx( $genome, $location );
-            printf " " x 16 . "Location: %s; Length %d\n", $location, length $seq;
-            if ( $info->{chr_strand} ne "+" ) {
-                $seq = revcom($seq);
-            }
-            push @seqs, $seq;
-        }
-        my ( $l_min, $l_max ) = minmax( map { length $_ } @seqs );
-        my $diff_ratio = sprintf "%.3f", $l_min / $l_max;
-        print " " x 12 . "Ratio[$diff_ratio]\tMin [$l_min]\tMax[$l_max]\n";
-
-        if ( $diff_ratio < 0.8 ) {
-            print " " x 12 . "*** Don't align this cc. Check it.\n";
-            push @bad_cc, $c;
-            next;
-        }
-
-        # aligning
-        print " " x 12, "Realign with $msa\n";
-        my $realigned_seqs = multi_align( \@seqs, $msa );
-
-        # Chopping hanging parts
-        print " " x 12, "Chopping hanging parts\n";
-        my $seq_of = {};
-        for my $i ( 0 .. $#heads ) {
-            $seq_of->{ $heads[$i] } = uc $realigned_seqs->[$i];
-        }
-
-        my ( $head_chopped, $tail_chopped ) = trim_head_tail( $seq_of, \@heads, 10, 10 );
-
-        my ( $new_seq_of, $new_heads )
-            = change_name_chopped( $seq_of, \@heads, $head_chopped, $tail_chopped );
-        printf " " x 16 . "Chopping %d %d\n", max( values %$head_chopped ),
-            max( values %$tail_chopped );
-
-        # names changed
-        push @new_cc, $new_heads;
-
-        # Write sequences
-        print " " x 12, "Write sequences\n";
-        for my $n ( @{$new_heads} ) {
-            path("$output.copy$copy.fas")->append(">$n\n");
-            path("$output.copy$copy.fas")->append( $new_seq_of->{$n} . "\n" );
-        }
-        path("$output.copy$copy.fas")->append("\n");
-
-        # Write pairwise alignments
-        print " " x 12, "Write pairwise alignments\n";
-        my $pair_ary = best_pairwise( $new_seq_of, $new_heads );
-        for my $p ( @{$pair_ary} ) {
-            my $realigned_pair
-                = multi_align( [ $new_seq_of->{ $p->[0] }, $new_seq_of->{ $p->[1] } ], $msa );
-            for my $i ( 0 .. scalar @{$p} - 1 ) {
-                path("$output.pairwise.fas")->append( ">" . $p->[$i] . "|copy=" . $copy . "\n" );
-                path("$output.pairwise.fas")->append( uc( $realigned_pair->[$i] ) . "\n" );
-            }
-            path("$output.pairwise.fas")->append("\n");
-        }
-    }
-
-    DumpFile("BAD_cc.yml", \@bad_cc);
-    
-    # we don't sort @new_cc
-    @cc = @new_cc;
-    print "\n";
-    
-}
-
-#----------------------------#
-# runlist
-#----------------------------#
-# per copy and chr
-my $set_copy_of = {};
-for my $c (@cc) {
+    my $c    = $chunk_ref->[0];
+    my $wid  = MCE->wid;
     my $copy = scalar @{$c};
-    if ( !exists $set_copy_of->{$copy} ) {
-        $set_copy_of->{$copy} = {};
-    }
-    for ( @{$c} ) {
-        my ( $chr, $set, $strand ) = string_to_set($_);
-        if ( !exists $set_copy_of->{$copy}{$chr} ) {
-            $set_copy_of->{$copy}{$chr} = AlignDB::IntSpan->new;
+
+    print "* Process cc [$chunk_id/$total_number] with [$copy] copies by worker #$wid\n";
+
+    # Get subseq from genomic files
+    print " " x 4 . "Get sequences from genomic files\n";
+    my @heads = @{$c};
+    my @seqs;
+    for my $head ( @{$c} ) {
+        my $info     = decode_header($head);
+        my $location = sprintf "%s:%d-%d", $info->{chr_name}, $info->{chr_start}, $info->{chr_end};
+        my $seq      = get_seq_faidx( $genome, $location );
+        printf " " x 8 . "Location: %s; Length %d\n", $location, length $seq;
+        if ( $info->{chr_strand} ne "+" ) {
+            $seq = revcom($seq);
         }
-        $set_copy_of->{$copy}{$chr}->add($set);
+        push @seqs, $seq;
     }
-}
+    my ( $l_min, $l_max ) = minmax( map { length $_ } @seqs );
+    my $diff_ratio = sprintf "%.3f", $l_min / $l_max;
+    print " " x 4 . "Ratio[$diff_ratio]\tMin [$l_min]\tMax[$l_max]\n";
 
-for my $key_i ( keys %{$set_copy_of} ) {
-    for my $key_j ( keys %{ $set_copy_of->{$key_i} } ) {
-        $set_copy_of->{$key_i}{$key_j}
-            = $set_copy_of->{$key_i}{$key_j}->runlist;
+    if ( $diff_ratio < $ratio ) {
+        print " " x 4 . "*** Don't align this cc. Check it.\n";
+        path($file_bad_cc)->append( Dump($c) );
+        next;
     }
-}
 
-print "Write chopped cc\n";
-DumpFile(
-    "$output.yml",
-    {   count   => $count_of,
-        cc      => \@cc,
-        runlist => $set_copy_of,
+    # aligning
+    print " " x 4, "Realign with $msa\n";
+    my $realigned_seqs = multi_align( \@seqs, $msa );
+
+    # Chopping hanging parts
+    print " " x 4, "Chopping hanging parts\n";
+    my $seq_of = {};
+    for my $i ( 0 .. $#heads ) {
+        $seq_of->{ $heads[$i] } = uc $realigned_seqs->[$i];
     }
-);
 
-# per chr
-my $set_chr_of = {};
+    my ( $head_chopped, $tail_chopped ) = trim_head_tail( $seq_of, \@heads, 10, 10 );
 
-for my $c (@cc) {
-    for ( @{$c} ) {
-        my ( $chr, $set, $strand ) = string_to_set($_);
-        if ( !exists $set_chr_of->{$chr} ) {
-            $set_chr_of->{$chr} = AlignDB::IntSpan->new;
+    printf " " x 8 . "Chopping %d %d\n", max( values %$head_chopped ), max( values %$tail_chopped );
+    my ( $new_seq_of, $new_heads )
+        = change_name_chopped( $seq_of, \@heads, $head_chopped, $tail_chopped );
+
+    # Write sequences
+    print " " x 4, "Write sequences\n";
+    my $str = "";
+    for my $n ( @{$new_heads} ) {
+        $str = ">$n\n";
+        $str .= uc( $new_seq_of->{$n} ) . "\n";
+    }
+    $str .= "\n";
+    path("$output.cc.copy$copy.fas")->append($str);
+
+    # Write pairwise alignments
+    print " " x 4, "Write pairwise alignments\n";
+    my $pair_ary = best_pairwise( $new_seq_of, $new_heads );
+    $str = "";
+    for my $p ( @{$pair_ary} ) {
+        my $realigned_pair
+            = multi_align( [ $new_seq_of->{ $p->[0] }, $new_seq_of->{ $p->[1] } ], $msa );
+        for my $i ( 0 .. scalar @{$p} - 1 ) {
+            $str .= ">" . $p->[$i] . "|copy=" . $copy . "\n";
+            $str .= uc( $realigned_pair->[$i] ) . "\n";
         }
-        $set_chr_of->{$chr}->add($set);
+        $str .= "\n";
     }
-}
-for my $key_i ( keys %{$set_chr_of} ) {
-    $set_chr_of->{$key_i} = $set_chr_of->{$key_i}->runlist;
-}
+    path("$output.cc.pairwise.fas")->append($str);
 
-DumpFile( "$output.chr.runlist.yml", $set_chr_of );
+    # names changed
+    print " " x 4, "Gather new heads for cc\n";
+    MCE->gather($new_heads);
+};
+
+MCE::Flow::init {
+    chunk_size  => 1,
+    max_workers => $parallel,
+};
+my @cc_new = mce_flow $worker, \@cc;
+MCE::Flow::finish;
+
+$stopwatch->block_message("Write file [$output.cc.yml]");
+@cc_new = sort_cc(@cc_new);
+DumpFile( "$output.cc.yml", \@cc_new );
+
+if ( path($file_bad_cc)->is_file ) {
+    $stopwatch->block_message("There are bad cc. Check [$file_bad_cc]");
+}
 
 $stopwatch->end_message;
 exit;

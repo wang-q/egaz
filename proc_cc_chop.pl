b9adbc30906078c95ac9cf25c882140e047d9669
wang-q <wang-q@outlook.com>
2016-05-26 23:21:06 +0800

    Unfinished

--- proc_cc_chop.pl
+++ proc_cc_chop.pl
@@ -17,7 +17,7 @@ use MCE::Flow Sereal => 1;
 
 use AlignDB::IntSpan;
 use AlignDB::Stopwatch;
-use AlignDB::Util qw(multi_align multi_align_matrix trim_head_tail);
+use AlignDB::Util qw(multi_align multi_align_matrix);
 
 use lib "$FindBin::RealBin/lib";
 use MyUtil
@@ -245,6 +245,82 @@ sub best_pairwise {
     }
 
     return \@pair_ary;
+}
+
+
+#----------------------------#
+# trim head and tail indels
+#----------------------------#
+#  If head length set to 1, the first indel will be trimmed
+#  Length set to 5 and the second indel will also be trimmed
+#   GAAA--C
+#   --AAAGC
+#   GAAAAGC
+sub trim_head_tail {
+    my $seq_of      = shift;
+    my $seq_names   = shift;
+    my $head_length = shift;    # indels in this region will also be trimmed
+    my $tail_length = shift;    # indels in this region will also be trimmed
+
+    # default value means only trim indels starting at the first base
+    $head_length = defined $head_length ? $head_length : 1;
+    $tail_length = defined $tail_length ? $tail_length : 1;
+
+    my $seq_number   = scalar @{$seq_names};
+    my $align_length = length $seq_of->{ $seq_names->[0] };
+
+    my $align_set = AlignDB::IntSpan->new("1-$align_length");
+    my $indel_set = AlignDB::IntSpan->new;
+
+    for my $n ( @{$seq_names} ) {
+        my $seq_indel_set = find_indel_set( $seq_of->{$n} );
+        $indel_set->merge($seq_indel_set);
+    }
+
+    # record bp chopped
+    my %head_chopped = map { $_ => 0 } @{$seq_names};
+    my %tail_chopped = map { $_ => 0 } @{$seq_names};
+
+    # There're no indels at all
+    return ( \%head_chopped, \%tail_chopped ) if $indel_set->is_empty;
+
+    # head indel(s) to be trimmed
+    my $head_set = AlignDB::IntSpan->new;
+    $head_set->add_range( 1, $head_length );
+    my $head_indel_set = $indel_set->find_islands($head_set);
+
+    # head indels
+    if ( $head_indel_set->is_not_empty ) {
+        for my $i ( 1 .. $head_indel_set->max ) {
+            my @column;
+            for my $n ( @{$seq_names} ) {
+                my $base = substr( $seq_of->{$n}, 0, 1, '' );
+                if ( $base ne '-' ) {
+                    $head_chopped{$n}++;
+                }
+            }
+        }
+    }
+
+    # tail indel(s) to be trimmed
+    my $tail_set = AlignDB::IntSpan->new;
+    $tail_set->add_range( $align_length - $tail_length + 1, $align_length );
+    my $tail_indel_set = $indel_set->find_islands($tail_set);
+
+    # tail indels
+    if ( $tail_indel_set->is_not_empty ) {
+        for my $i ( $tail_indel_set->min .. $align_length ) {
+            my @column;
+            for my $n ( @{$seq_names} ) {
+                my $base = substr( $seq_of->{$n}, -1, 1, '' );
+                if ( $base ne '-' ) {
+                    $tail_chopped{$n}++;
+                }
+            }
+        }
+    }
+
+    return ( \%head_chopped, \%tail_chopped );
 }
 
 __END__
